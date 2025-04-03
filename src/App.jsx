0cdbbec5648b04e1f3c188e1ffba52188cca2a32
Latiful Rahman <latif.fiverr@gmail.com>
2025-04-03 14:42:24 +0000

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+
+function App() {
+
+
+  return (
+    <>
+      <div class="bg-grid min-h-screen bg-gradient-to-br from-blue-900 via-purple-900 to-teal-900"></div>
+
+    </>
+  )
+}
+
+export default App
